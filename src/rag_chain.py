ad7da74aa009b9385bee9e98f0ee7b348bfc79a6
AMITS-LAPTOP\amits <amit39836@gmail.com>
2026-06-22 12:49:05 +0530

    updated

--- src/rag_chain.py
+++ src/rag_chain.py
@@ -1,6 +1,5 @@
-# from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnableParallel,RunnablePassthrough,RunnableLambda
+from langchain_core.runnables import RunnablePassthrough,RunnableLambda
 from langchain_core.output_parsers import StrOutputParser
 from langchain_groq import ChatGroq
 
